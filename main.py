36042bbb58f0b1b1ff00ca546709dfda49db783a
pro-code-designer <prodesigner7979@gmail.com>
2023-02-05 11:30:29 +0330

    add turn

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+# -------------------------------------------------------------------------------------------------------------------------------
+# The first turn is determined randomly by generating a random integer value between 1 and 2
+# If turn is 1, player's turn and 0 stands for computer's turn
+turn = random.randint(1, 2)
